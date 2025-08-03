23a25a8300936c6c98d2e24354a0d57ffe59f2b2
jahongirovich <abdubositabduqahhorov2011@gmail.com>
2025-08-03 23:37:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let arr = [2, 4, 9, 10, 45, 67, 8, 90];
+console.log(arr, 'boshlanishdagi array');
+
+
+let evenNumbers = arr.filter(num => num % 2 === 0);
+console.log(evenNumbers, 'juft sonlarga ajratilgan array');
